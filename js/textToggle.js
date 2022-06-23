ca46bea5b37f2a162e82a28abe114bbdf4ef59c4
florencgmz <florencgmz@gmail.com>
2022-06-22 22:06:43 -0300

    cambio de texto en hover con jquery

---
+++ js/textToggle.js
@@ -0,0 +1,26 @@
+$('#text-green')
+    .data('textToggle', "!@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n !@#W$9876543210?!abc;:+=-,._ \n ")
+    .hover(function (e) {
+        var that = $(this);
+        
+        // get the text from data attribute
+        var textToSet = that.data('textToggle');
+        
+        // save the current text so it can be reverted
+        that.data('textToggle', that.text());
+        
+        // set the new text
+        that.text(textToSet);
+    }, function (e) {
+        var that = $(this);
+        
+        // get the text from data attribute
+        var textToSet = that.data('textToggle');
+        
+        // save the current text so it can be reverted
+        that.data('textToggle', that.text());
+        
+        // set the new text
+        that.text(textToSet);
+    });
+    
